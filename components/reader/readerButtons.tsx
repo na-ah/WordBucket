47af9bf21acb7c0dc361df5be99250a3858d6704
na-ah <na.ah.0006@gmail.com>
2024-09-16 15:03:03 +0900

    style: /readerのボタンを調整

--- components/reader/readerButtons.tsx
+++ components/reader/readerButtons.tsx
@@ -16,42 +16,48 @@ export default function ReaderButtons({
 }: ReaderButtonProps) {
   return (
     <>
-      <button
-        className="text-3xl"
-        onClick={paragraphIndexDecrease}
-      >
-        <MdOutlineKeyboardDoubleArrowLeft />
-      </button>
-      <button
-        className="text-3xl"
-        onClick={sentenceIndexDecrease}
-      >
-        <MdOutlineKeyboardArrowLeft />
-      </button>
-      <button>
-        <a
-          href={`${
-            "https://www.deepl.com/ja/translator#en/ja/" +
-            encodeURIComponent(currentSentence)
-          }`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SiDeepl className="text-5xl text-yellow-300" />
-        </a>
-      </button>
-      <button
-        className="text-3xl"
-        onClick={sentenceIndexIncrease}
-      >
-        <MdOutlineKeyboardArrowRight />
-      </button>
-      <button
-        className="text-3xl"
-        onClick={paragraphIndexIncrease}
-      >
-        <MdOutlineKeyboardDoubleArrowRight />
-      </button>
+      <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-full flex justify-around h-16">
+          <button
+            className="text-3xl"
+            onClick={paragraphIndexDecrease}
+          >
+            <MdOutlineKeyboardDoubleArrowLeft />
+          </button>
+          <button>
+            <a
+              href={`${
+                "https://www.deepl.com/ja/translator#en/ja/" +
+                encodeURIComponent(currentSentence)
+              }`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SiDeepl className="text-5xl text-yellow-300" />
+            </a>
+          </button>
+          <button
+            className="text-3xl"
+            onClick={paragraphIndexIncrease}
+          >
+            <MdOutlineKeyboardDoubleArrowRight />
+          </button>
+        </div>
+        <div className="w-full flex justify-around h-16">
+          <button
+            className="text-3xl"
+            onClick={sentenceIndexDecrease}
+          >
+            <MdOutlineKeyboardArrowLeft />
+          </button>
+          <button
+            className="text-3xl"
+            onClick={sentenceIndexIncrease}
+          >
+            <MdOutlineKeyboardArrowRight />
+          </button>
+        </div>
+      </div>
     </>
   );
 }
