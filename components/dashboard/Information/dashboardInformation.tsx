7625b319a12f90b687848e5bbf51fab5eb63e42c
na-ah <na.ah.0006@gmail.com>
2024-09-16 20:06:29 +0900

    feat: /dashboardの意味未登録カードから登録画面に遷移可能に

--- components/dashboard/Information/dashboardInformation.tsx
+++ components/dashboard/Information/dashboardInformation.tsx
@@ -1,5 +1,6 @@
 import LeftBarTitle from "@/components/shared/LeftBarTitle";
 import { DashboardInformationProps } from "@/types/types";
+import Link from "next/link";
 
 export default function DashboardInformation({
   informations,
@@ -11,11 +12,26 @@ export default function DashboardInformation({
           key={i}
           className="mt-3 flex justify-between"
         >
-          <LeftBarTitle
-            title={information.title}
-            color={information.color}
-          />
-          <span>{information.text}</span>
+          {information.title === "意味未登録カード" ? (
+            <Link
+              href={"/word/missing/meaning"}
+              className="flex justify-between w-full"
+            >
+              <LeftBarTitle
+                title={information.title}
+                color={information.color}
+              />
+              <span>{information.text}</span>
+            </Link>
+          ) : (
+            <>
+              <LeftBarTitle
+                title={information.title}
+                color={information.color}
+              />
+              <span>{information.text}</span>
+            </>
+          )}
         </div>
       ))}
     </>
