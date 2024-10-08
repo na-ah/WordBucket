322084f579dc3e46f8432fda50c88b0e34d14ce8
na-ah <na.ah.0006@gmail.com>
2024-10-08 13:10:04 +0900

    feat: 最大幅を固定

--- components/Template/Area/MainArea.tsx
+++ components/Template/Area/MainArea.tsx
@@ -4,7 +4,9 @@ export default function Main({ children }: { children: ReactNode }) {
   return (
     <>
       <div className="text-white bg-zinc-800 w-full flex flex-col">
-        <div className="flex-1 flex flex-col max-w-[800px]">{children}</div>
+        <div className="flex-1 flex flex-col max-w-[500px] w-full mx-auto">
+          {children}
+        </div>
       </div>
     </>
   );
--- components/Template/Layout/Layout.tsx
+++ components/Template/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import MainArea from "../Area/MainArea";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <div className="flex min-h-dvh">
+      <div className="flex min-h-dvh min-w-[350px]">
         <Sidebar />
         <MainArea>{children}</MainArea>
       </div>
